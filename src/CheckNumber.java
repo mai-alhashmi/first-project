9a69e8fa5fce37b690908e04c2282153effc70d2
Mai <mimiwarsaw@gmail.com>
2024-03-11 11:29:18 +0100

    ADD checNumber

---
+++ src/CheckNumber.java
@@ -0,0 +1,10 @@
+public class CheckNumber {
+    public static void main(String[] args) {
+        System.out.printf("total average %d %n", getAverage(33, 266, 55));
+    }
+
+    public static int getAverage(int number1, int number2, int number3) {
+        return (number1 + number2 + number3) / 3;
+
+    }
+}
