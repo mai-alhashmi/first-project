8a1ba449a11ff602b59b6af38c92e1f15643dbda
Mai <mimiwarsaw@gmail.com>
2024-03-12 09:06:52 +0100

    ADD files

--- src/CheckNumberTwo.java
+++ src/CheckNumberTwo.java
@@ -3,18 +3,22 @@ import java.util.Scanner;
 public class CheckNumberTwo {
     public static void main(String[] args) {
 
+    }
+
+    public static void isEven() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("enter number");
+        int number = scanner.nextInt();
+        isEven(number);
+
+
+    }
+
+    public static void isEven(int number) {
+        if (number % 2 == 0) {
+            System.out.printf(number + "is even");
+        } else {
+            System.out.printf(number + "is ood");
         }
-        public static void isEven(){
-            Scanner scanner =new Scanner(System.in);
-            System.out.printf("enter number");
-            int number=scanner.nextInt();
-            if (number % 2==0) {
-                System.out.printf(number + "is even");
-            }else {
-                System.out.printf(number + "is ood");
-            }
-
-
-
     }
 }
